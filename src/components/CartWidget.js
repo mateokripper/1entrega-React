1536adf7080a43153eb6ba57cd574cc0594a06d1
Mateo Kripper <mateokripper27@gmail.com>
2022-08-25 16:38:35 -0300

    agregando components

---
+++ src/components/CartWidget.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
+import './styles/CartWidget.css';
+
+const CartWidget = () => {
+  //la lógica va siempre antes del return
+  return (
+    <div className="cart-widget">
+      <FontAwesomeIcon icon={faCartShopping} size="2x" color="white" />
+      <div className="qty-display">0</div>
+    </div>
+  );
+};
+
+export default CartWidget;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import CartWidget from './CartWidget';
+import ItemListContainer from './ItemListContainer';
 
 function NavBar () {
     return (
@@ -13,9 +15,12 @@ function NavBar () {
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#features">Features</Nav.Link>
                 <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <CartWidget/>
               </Nav>
             </Container>
+            
           </Navbar>
+          <ItemListContainer greetings = 'Welcome'></ItemListContainer> 
          
           
         </>
